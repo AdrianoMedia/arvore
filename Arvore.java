f5de64bd92529d5b70e3679ef069f2778317212d
Adriano Gabriel Girardi <105896577+AdrianoMedia@users.noreply.github.com>
2023-05-19 09:08:35 -0300

    Add files via upload

---
+++ Arvore.java
@@ -0,0 +1,65 @@
+public class Arvore<T> {
+
+    private NoArvore<T> raiz;
+
+    //de outras classes
+    public Arvore() {
+		super();
+        raiz = null;
+	}
+
+    protected void setRaiz(NoArvore<T> raiz) {
+		this.raiz = raiz;
+	}
+    public NoArvore<T> getRaiz() {
+        return this.raiz;
+    }
+
+    public boolean vazia() {
+		if (raiz == null) {
+			return true;
+		}
+		return false;
+	}
+
+    //autoral
+
+    public NoArvore<T> pertence(T info) {
+        if(this.vazia()) {
+            return null;
+        }
+        return this.raiz.pertence(info);
+    }
+
+    public String toString() {
+        if(this.vazia()) {
+            return "<>";
+        } 
+        return this.raiz.imprimePre();
+    }
+
+    //trabalho 2
+    public int getAltura() {
+        if(this.vazia()) {
+            return -1;
+        } else {
+            return raiz.getAltura(raiz);
+        }
+    }
+
+    public int getNivel(T info) {
+        if(this.vazia()) {
+            return -1;
+        } else {
+            return raiz.getNivel(info, raiz, 1);   
+             }
+    }
+
+    public boolean isBalanceada() {
+        int alturaMaxima = raiz.calcularAltura(raiz);
+        return alturaMaxima != -1;
+    }
+    
+
+    
+}
---
+++ NoArvore.java
@@ -0,0 +1,123 @@
+public class NoArvore<T> {
+
+    private T info;
+    private NoArvore<T> filho;
+    private NoArvore<T> irmao;
+
+    public T getInfo() {
+        return this.info;
+    }
+
+    public NoArvore<T> getFilho() {
+        return this.filho;
+    }
+
+    public NoArvore<T> getIrmao() {
+        return this.irmao;
+    }
+
+    public NoArvore(T info) {
+        super();
+        this.info = info;
+        this.filho = null;
+        this.irmao = null;
+    }
+
+    public void inserirFilho(NoArvore<T> sa) {
+        sa.irmao = this.filho;
+        this.filho = sa;
+    }
+
+    public NoArvore<T> pertence(T info) {
+
+        if (this.info.equals(info)) {
+            return this;
+        } else {
+            NoArvore<T> no = null;
+            if (this.getFilho() != null) {
+                no = this.filho.pertence(info);
+            }
+            if (no == null && this.irmao != null) {
+                no = this.irmao.pertence(info);
+            }
+            return no;
+        }
+
+    }
+
+    public String imprimePre() {
+        String str = "<" + this.info.toString();
+        if (this.filho != null) {
+            str += this.filho.imprimePre();
+        }
+        str += ">";
+        if (this.irmao != null) {
+            str += this.irmao.imprimePre();
+        }
+        return str;
+    }
+
+    // trabalho 2
+    public int getAltura(NoArvore<T> no) {
+        if (no == null) {
+            return 0;
+        } else {
+            int alturaFilhos = 0;
+            NoArvore<T> filho = no.filho;
+
+            while (filho != null) {
+                int alturaFilho = getAltura(filho);
+                alturaFilhos = Math.max(alturaFilhos, alturaFilho);
+                filho = filho.irmao;
+            }
+            return alturaFilhos + 1;
+        }
+    }
+
+    public int getNivel(T info, NoArvore<T> no, int nivelAtual) {
+        if (no == null) {
+            return -1;
+        } else {
+            if (no.info.equals(info)) {
+                return nivelAtual;
+            } else {
+                NoArvore<T> filho = no.filho;
+
+                while (filho != null) {
+                    int nivelFilho = getNivel(info, filho, nivelAtual+1);
+                    if (nivelFilho != -1) {
+                        return nivelFilho;
+                    }
+                    filho = filho.irmao;
+                }
+                return -1;
+            }
+        }
+    }
+
+    public int calcularAltura(NoArvore<T> no) {
+        if (no == null) {
+            return 0;
+        }
+
+        int alturaMaxima = -1;
+        NoArvore<T> filho = no.filho;
+        while (filho != null) {
+            int alturaFilho = calcularAltura(filho);
+            if (alturaFilho == -1) {
+                return -1; // Se algum filho for desbalanceado, a árvore toda é desbalanceada
+            }
+            if (alturaMaxima == -1) {
+                alturaMaxima = alturaFilho; // Inicializa a altura máxima com o primeiro filho válido
+            } else if (Math.abs(alturaFilho - alturaMaxima) > 1) {
+                return -1; // Se algum filho tiver altura muito diferente, a árvore é desbalanceada
+            } else {
+                alturaMaxima = Math.max(alturaMaxima, alturaFilho);
+            }
+            filho = filho.irmao;
+        }
+
+        return alturaMaxima + 1;
+    }
+
+}
